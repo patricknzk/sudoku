1d18689d81aa8a2c11380c8cc62d76b7e216b57d
patricknzk <139427149+patricknzk@users.noreply.github.com>
2024-08-15 21:12:02 +1000

    valid board function created

--- src/board.py
+++ src/board.py
@@ -19,13 +19,34 @@ class Board:
         # value has to be unique to that subgrid
         pass
 
+    def is_valid_subgrid(self, board):
+        # Check all 3x3 subgrids
+        for start_row in range(0, 9, 3):
+            for start_col in range(0, 9, 3):
+                subgrid = []
+                for i in range(3):
+                    for j in range(3):
+                        val = board[start_row + i][start_col + j]
+                        if val != 0:
+                            subgrid.append(val)
+                if len(subgrid) != len(set(subgrid)):
+                    return False
+        return True
+
     def is_valid_board(self, board):
-        # for the value in a cell, the entire row and entire column cannot contain the same value.
+        # for the value in a cell, the entire row and entire column cannot
+        # contain the same value.
         for i in range(9):
-            if len([n for n in i if n != 0]) != len(set(board[i])):
+            row = [n for n in board[i] if n != 0]
+            if len(row) != len(set(row)):
                 return False
 
         for j in range(9):
-            if len([n for n in j if n != 0]) != len(set(board[j])):
+            column = [board[i][j] for i in range(9) if board[i][j] != 0]
+            if len(column) != len(set(column)):
                 return False
-        pass
+        
+        if not self.is_valid_subgrid(board):
+            return False
+
+        return True
